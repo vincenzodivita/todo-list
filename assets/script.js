f14a0cc917d0dfa5a97b0030dd899492b0bb6bc4
vincenzodivita <divitavincenzo@icloud.com>
2023-07-08 17:59:56 +0200

    Add new li fn

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
 document.querySelector('button').addEventListener('click', function () {
-    document.getElementById('app').innerHTML = document.querySelector('input').value
+    document.getElementById('app').innerHTML += "<li>" + document.querySelector('input').value + "</li>"
 });
+
+
