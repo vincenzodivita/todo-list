49f214301ba634bf2d2a11d2145e346863e85b5c
vincenzodivita <divitavincenzo@icloud.com>
2023-07-08 18:07:58 +0200

    Add consts

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
-document.querySelector('button').addEventListener('click', function () {
-    document.getElementById('app').innerHTML += "<li>" + document.querySelector('input').value + "</li>"
+const btn = document.querySelector('button');
+const list = document.getElementById('app');
+const task = document.querySelector('input');
+
+btn.addEventListener('click', function () {
+    list.innerHTML += "<li>" + task.value + "</li>";
 });
 
 
